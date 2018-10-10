ddb826c01bd6e8a001bea343bb482b6130858a3d
Peter Teich <peter.teich@gmail.com>
2018-10-10 15:19:58 +0200

    initial release
    Initial release that allows search for data in Consul K/V with glob pattern (e.g. *data*) or regular expression pattern.

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/hashicorp/consul/api"
+	"github.com/jawher/mow.cli"
+	"github.com/pteich/consul-kv-search/search"
+)
+
+var Version string
+
+func main() {
+
+	app := cli.App("consul-kv-search", "CLI tool to search for data in Consul K/V store. https://github.com/pteich/consul-kv-search")
+	app.Version("v version", Version)
+	app.Spec = "[-a] [-t] [-p] [-g|-r] QUERY"
+
+	var (
+		configConsulAddr = app.StringOpt("a address", "127.0.0.1:8500", "Address and port of Consul server")
+		configToken      = app.StringOpt("t token", "", "Consul ACL token to use")
+		configPath       = app.StringOpt("p path", "/", "K/V path to start search")
+		configQueryGlob  = app.BoolOpt("g glob", true, "Query interpreted as glob pattern")
+		configQueryRegex = app.BoolOpt("r regex", false, "Query interpreted as regular expression")
+		configQuery      = app.StringArg("QUERY", "*", "Search query")
+	)
+
+	app.Action = func() {
+		fmt.Println(*configConsulAddr, *configToken, *configPath, *configQueryGlob, *configQueryRegex, *configQuery)
+
+		consulConfig := api.DefaultConfig()
+		consulConfig.Address = *configConsulAddr
+		consulConfig.Token = *configToken
+
+		consulClient, err := api.NewClient(consulConfig)
+		if err != nil {
+			fmt.Printf("Could not connect to Consul at %s - %v\n", *configConsulAddr, err)
+			os.Exit(1)
+		}
+
+		consulSearch := search.NewConsulSearch(consulClient)
+		var foundPairs []search.ResultPair
+
+		if *configQueryRegex {
+			foundPairs, err = consulSearch.SearchRegex(*configQuery, *configPath)
+		} else {
+			foundPairs, err = consulSearch.SearchGlob(*configQuery, *configPath)
+		}
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+		for _, element := range foundPairs {
+			fmt.Fprintf(w, "%s\t%s\n", element.Key, element.Value)
+			fmt.Fprintln(w)
+		}
+		w.Flush()
+
+	}
+
+	app.Run(os.Args)
+}
---
+++ search/search.go
@@ -0,0 +1,76 @@
+package search
+
+import (
+	"regexp"
+
+	"github.com/gobwas/glob"
+	"github.com/hashicorp/consul/api"
+	"github.com/pkg/errors"
+)
+
+type ResultPair struct {
+	Key   string
+	Value string
+}
+
+type ConsulSearch struct {
+	consulClient *api.Client
+}
+
+func (cs *ConsulSearch) SearchGlob(query string, path string) ([]ResultPair, error) {
+
+	foundPairs := make([]ResultPair, 0)
+	pattern := glob.MustCompile(query)
+
+	pairs, err := cs.getKVPairs(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not get K/V pairs in glob search")
+	}
+
+	for _, element := range pairs {
+		if pattern.Match(string(element.Value)) {
+			pair := ResultPair{
+				Key:   string(element.Key),
+				Value: string(element.Value),
+			}
+			foundPairs = append(foundPairs, pair)
+		}
+	}
+
+	return foundPairs, nil
+}
+
+func (cs *ConsulSearch) SearchRegex(query string, path string) ([]ResultPair, error) {
+	foundPairs := make([]ResultPair, 0)
+	pattern := regexp.MustCompile(query)
+
+	pairs, err := cs.getKVPairs(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not get K/V pairs in regex search")
+	}
+
+	for _, element := range pairs {
+		if pattern.Match(element.Value) {
+			pair := ResultPair{
+				Key:   string(element.Key),
+				Value: string(element.Value),
+			}
+			foundPairs = append(foundPairs, pair)
+		}
+	}
+
+	return foundPairs, nil
+}
+
+func (cs *ConsulSearch) getKVPairs(path string) (api.KVPairs, error) {
+	kv := cs.consulClient.KV()
+	pairs, _, err := kv.List(path, nil)
+
+	return pairs, err
+}
+
+func NewConsulSearch(consulClient *api.Client) *ConsulSearch {
+	return &ConsulSearch{
+		consulClient: consulClient,
+	}
+}
